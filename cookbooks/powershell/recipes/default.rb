0320e5674a9ee7b47280ef362601424f5a6be2d3
Ewan Nisbet <ewan.nisbet@huddle.local>
2016-10-08 05:17:00 +0100

    nx module & dependency install on windows & ubuntu

--- cookbooks/powershell/recipes/default.rb
+++ cookbooks/powershell/recipes/default.rb
@@ -23,6 +23,23 @@ if node[:platform].include?("ubuntu")
       #without any custom mappings.
       package_name 'python-minimal'
     end
+    package "Install libcurl4-openssl-dev" do
+      #If this is missing, when we try:
+      #Install-module nx
+      #we get errors like:
+      #PackageManagement\Install-Package : No match was found for the specified
+      #search criteria and module name 'nx'. Try Get-PSRepository to see all
+      # available registered module repositories
+      #if its working we can expect something like:
+      #Get-PSRepository
+      #Name                      InstallationPolicy   SourceLocation
+      #----                      ------------------   --------------
+      #PSGallery                 Untrusted            https://www.powershellgallery.com/api/v2/
+      #solution found on https://github.com/Microsoft/vsts-agent/issues/232
+      package_name 'libcurl4-openssl-dev'
+    end
+.
+    apt-get install libcurl4-openssl-dev
     remote_file "/var/tmp/omi-1.1.0.ssl_100.x64.deb" do
       #source "#{node[:mye_deb_source_url]}"
       source 'https://github.com/Microsoft/omi/releases/download/v1.1.0-0/omi-1.1.0.ssl_100.x64.deb'
@@ -90,8 +107,9 @@ if node[:platform].include?("ubuntu")
 #sudo powershell "install-module nx -Force"
 
   end
+#https://msdn.microsoft.com/en-us/powershell/dsc/lnxgettingstarted
+
 #https://github.com/PowerShell/PowerShell/releases/tag/v6.0.0-alpha.10
 #http://www.howtogeek.com/267858/how-to-install-microsoft-powershell-on-linux-or-os-x/
-
 #http://www.systemcentercentral.com/day-7-install-dsc-providers-linux-ubuntu-12-04/
 end
