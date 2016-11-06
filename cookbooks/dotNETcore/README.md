b65632908b00abce6e7ec10a563a9ea6f03c9fe7
Ewan Nisbet <ewan.nisbet@huddle.local>
2016-11-06 17:00:11 +0000

    add dotNETcore cookbook

---
+++ cookbooks/dotNETcore/README.md
@@ -0,0 +1,4 @@
+# dotNETcore
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/dotNETcore/recipes/default.rb
@@ -0,0 +1,30 @@
+#
+# Cookbook Name:: dotNETcore
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+case node[:platform]
+when "ubuntu"
+    case node[:platform_version]
+    #common prereqs
+    when "14.04","16.04"
+        apt_repository "dotnet-release-trusty" do
+            uri "https://apt-mo.trafficmanager.net/repos/dotnet-release"
+            distribution node['lsb']['codename']
+            components ["main"]
+            keyserver "apt-mo.trafficmanager.net"
+            key "417A0893"
+            notifies :run, resources(:execute => "apt-get-update"), :immediately
+        end
+        execute "apt-get-update" do
+            command "apt-get update"
+            ignore_failure true
+            action :nothing
+        end
+        package "dotNETcore" do
+            package_name 'dotnet-dev-1.0.0-preview2-003131'
+        end
+    end
+end
+
